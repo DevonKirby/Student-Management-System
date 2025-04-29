6a0028beea0c2b3382738fc6bc3d38144f704199
Devon Kirby <devonlkirby@gmail.com>
2025-04-29 08:16:52 -0500

    Separated app and index

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,6 @@
-const express = require('express');
+const app = require('./app');
+const PORT = process.env.PORT || 5000;
 
-const app = express();
-const port = 3000;
-
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
